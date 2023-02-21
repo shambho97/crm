d442b1abf4d1eabc68d72baaeaac0b16bc0af627
shambho97 <125727761+shambho97@users.noreply.github.com>
2023-02-21 13:11:32 +0530

    Add files via upload

---
+++ TwinPrimeNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class TwinPrimeNumber
+{
+  public static void main(String[]args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the number");
+    int num=sc.nextInt();
+    System.out.println("Enter the number");
+    int num2=sc.nextInt();
+    System.out.println(prime num==prime num2);
+  }
+  public static void prime(int num2)
+  {
+    int count=0;
+    for(int i=1;i<=num;i++)
+    if(num%i==0)
+    count++
+  }
+   if(count==2)
+   {
+   return ture
+   }
+   else
+   {
+     System.out.println("it is not twinprime")
+   }
+  
+}
